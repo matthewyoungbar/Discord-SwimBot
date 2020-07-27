247b38da7116119e3c3d344fb1b120a5126ba75f
Michael Krasnitski <michael.krasnitski@gmail.com>
2020-07-27 19:33:19 -0400

    add collegeswimming scraping

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-import discord
+import requests
+import urllib.parse
+from discord.ext import commands
 
 TOKEN = "NzM3NDE5NDQwNjAxMTcwMDEw.Xx9FXw.yYzdbBSHkKR38Lukdd-tYIadqqM"
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def collegeswimming(ctx, *args):
+    base = 'https://www.collegeswimming.com'
+    url = base + '/api/search/?q=' + urllib.parse.quote(' '.join(args))
+    results = [obj for obj in requests.get(url).json() if 'swimmer' in obj['id']]
+    if len(results) == 1:
+        await ctx.send(base + results[0]['url'])
+    elif len(results) > 1:
+        swimmers = ""
+        for swimmer in results:
+            swimmers += f"{swimmer['name']} - "
+            if swimmer['team'] != '':
+                swimmers += f"{swimmer['team']}, "
+            swimmers += f"{swimmer['location']}\n"
+        await ctx.send("There were multiple results. Please provide a more specific search.\n```\n" + swimmers + '```')
+    elif len(results) == 0:
+        await ctx.send("There were no results. Either your search is too broard or too narrow.")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def cs(ctx, *args):
+    await collegeswimming(ctx, *args)
 
-client.run(TOKEN)
+bot.run(TOKEN)
