be28f61d8318ee7c8bfce5c2025f27e4aba45c36
Michael Krasnitski <michael.krasnitski@gmail.com>
2020-07-30 19:17:09 -0400

    update maintenance.sh

--- maintenence.sh
+++ maintenence.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-if [[ $# -eq 0 ]]
+if [[ $1 == "on" ]]
 then
-    echo "Usage: $0 on|off"
+    heroku maintenance:on -a discord-swimbot
+    heroku ps:scale worker=0 -a discord-swimbot
+elif [[ $1 == "off" ]]
+then
+    heroku maintenance:off -a discord-swimbot
+    heroku ps:scale worker=1 -a discord-swimbot
 else
-    if [[ $1 == "on" ]]
-    then
-        heroku maintenance:on -a discord-swimbot
-        heroku ps:scale worker=0 -a discord-swimbot
-    elif [[ $1 == "off" ]]
-    then
-        heroku maintenance:off -a discord-swimbot
-        heroku ps:scale worker=1 -a discord-swimbot
-    fi
+    echo "Usage: $0 on|off"
 fi
